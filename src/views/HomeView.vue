241881bb9a368fd990f57f650c296d79b083d81b
shini-leaf <shiniyou137@gmail.com>
2022-08-22 00:48:28 +0900

    Homeからのリンクをとりあえず作りました

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,22 @@
 <template>
   <h1>Home</h1>
-  <div>ここにそれぞれのアプリに行けるリンクを設置する</div>
+  <div class="link">
+    <router-link to="/quiz" class="link__home">クイズへ</router-link>
+    <router-link to="/pallete" class="link__home">パレットへ</router-link>
+    <router-link to="/memo" class="link__home">メモへ</router-link>
+    <router-link to="/typing" class="link__home">タイピングへ</router-link>
+  </div>
 </template>
+
+<style>
+.link {
+  display: flex;
+  flex-direction: column;
+}
+
+.link__home {
+  font-weight: bold;
+  font-size: 24px;
+  color: #2c3e50;
+}
+</style>
