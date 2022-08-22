815e3db88c780515d68ed1ef76f03b66120fd99a
shini-leaf <shiniyou137@gmail.com>
2022-08-22 23:10:56 +0900

    じゃんけんの機能だけつけました。見た目はまだです

--- src/views/JankenApp.vue
+++ src/views/JankenApp.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <h1>じゃんけんゲーム</h1>
-    <button v-for="choose in choices" v-bind:key="choose.number">
-      {{ choose.te }}
+    <button
+      v-for="choice in choices"
+      v-bind:key="choice.number"
+      v-on:click="choose(choice)"
+    >
+      {{ choice.te }}
     </button>
-    <p>{{ player }}</p>
-    <p>{{ pc }}</p>
-    <p>{{ result }}</p>
+    <p>{{ resultText }}</p>
+    <p>あなたは{{ player }}</p>
+    <p>パソコンは{{ pc }}</p>
   </div>
 </template>
 <script>
@@ -15,7 +19,7 @@ export default {
     return {
       player: "",
       pc: "",
-      result: "",
+      resultText: "",
       choices: [
         {
           te: "グー",
@@ -32,7 +36,28 @@ export default {
       ],
     }
   },
-  methods() {},
+  methods: {
+    choose(choice) {
+      this.player = choice.number
+      this.result()
+    },
+    result() {
+      //Math.random()は0~1未満の少数を返す
+      //最大値に3足して0~2にした
+      //math.floor()で小数点以下を切り捨て
+      this.pc = Math.floor(Math.random() * 3)
+      switch ((this.player - this.pc + 3) % 3) {
+        case 0:
+          this.resultText = "引き分け..."
+          break
+        case 1:
+          this.resultText = "残念！負けです💦"
+          break
+        case 2:
+          this.resultText = "おめでとう！勝ちです🎉"
+      }
+    },
+  },
 }
 </script>
 <style></style>
